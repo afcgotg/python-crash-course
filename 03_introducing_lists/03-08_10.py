402639da2c35f8734aa877f2a3b1b601cc361bc7
Alex Fernandez <fernialex@gmail.com>
2024-07-12 12:30:09 +0200

    3rd chapter

---
+++ 03_introducing_lists/03-08_10.py
@@ -0,0 +1,31 @@
+# 3-8. Seeing the World
+
+places = ["New Zeland", "Canary Islans", "Menorca", "New York", "Mexico"]
+print(places)
+print(sorted(places))
+print(places)
+print(sorted(places, reverse=True))
+print(places)
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
+
+# 3-9. Dinner Guests
+
+guests = ["shreck", "asno", "fiona", "gatO"]
+len(guests)
+
+# 3-10. Every Function
+
+prime_numbers = ["two", "three", "five", "seven", "eleven", "thirteen", "seventeen", "nineteen", "twenty three"]
+
+print(prime_numbers)
+print(sorted(prime_numbers))
+print(sorted(prime_numbers, reverse=True))
+prime_numbers.reverse()
+print(prime_numbers)
