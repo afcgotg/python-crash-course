6037a1c4ce2e4aa300afc880f713a95334fe3f25
Alex Fernandez <fernialex@gmail.com>
2024-07-15 13:13:36 +0200

    Files renamed

---
+++ 04_working_with_lists/04_13.py
@@ -0,0 +1,9 @@
+foods = ("pasta", "pizza", "salad", "meat", "fish")
+for food in foods:
+    print(food)
+
+#foods[1] = "burguer"
+
+foods = ("pasta", "burguer", "salad", "vegetables", "fish")
+for food in foods:
+    print(food)
